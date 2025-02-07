ff380cfe9452b8c5d21c0ed9d80a4513e25eaf3f
Luca Zuin <zuin.luca1111@gmail.com>
2025-02-07 12:05:01 +0100

    commit 3

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,16 +2,19 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 
 const TopBar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-dark" data-bs-theme="dark" style="background-color: #221f1f !important">
       <Container fluid>
         <Navbar.Brand href="#">
-          <img />
+          <img src="../assets/img/logo.png" style={{ width: "100px" }} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">Link</Nav.Link>
+            <Nav.Link href="#">TV Shows</Nav.Link>
+            <Nav.Link href="#">Movies</Nav.Link>
+            <Nav.Link href="#">Recently Added</Nav.Link>
+            <Nav.Link href="#">My List</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
