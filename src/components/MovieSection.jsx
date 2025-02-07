e857f76566bf8f332054e9fbbef8b138246ed830
Luca Zuin <zuin.luca1111@gmail.com>
2025-02-07 14:34:51 +0100

    commit 6

--- src/components/MovieSection.jsx
+++ src/components/MovieSection.jsx
@@ -3,11 +3,11 @@ import FetchMovies from "./FetchMovies";
 const MovieSection = ({ title, query }) => (
   <FetchMovies query={query}>
     {(movies) => (
-      <div className="container mt-4">
+      <div className="container-fluid mt-4">
         <h4>{title}</h4>
         <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4">
           {movies.map((movie) => (
-            <div className="col mb-2 text-center px-1" key={movie.imdbID}>
+            <div className="col mb-2 text-center px-0" key={movie.imdbID}>
               <img className="img-fluid" src={movie.Poster} alt={movie.Title} style={{ height: "300px" }} />
             </div>
           ))}
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,4 +1,5 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Search, Bell, PersonCircle } from "react-bootstrap-icons";
 
 const TopBar = () => {
   return (
@@ -9,21 +10,29 @@ const TopBar = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
+          <Nav className="">
             <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">TV Shows</Nav.Link>
+            <Nav.Link href="#" active>
+              TV Shows
+            </Nav.Link>
             <Nav.Link href="#">Movies</Nav.Link>
             <Nav.Link href="#">Recently Added</Nav.Link>
             <Nav.Link href="#">My List</Nav.Link>
           </Nav>
 
-          <div className="d-flex align-items-center">
-            <i className="bi bi-search icons"></i>
-            <div id="kids" className="fw-bold">
+          <div className="d-flex ms-auto align-items-center">
+            <a>
+              <Search size={20} color="white" className="mx-2" />
+            </a>
+            <div id="kids" className="fw-bold mx-2">
               KIDS
             </div>
-            <i className="bi bi-bell icons"></i>
-            <i className="bi bi-person-circle icons"></i>
+            <a>
+              <Bell size={20} color="white" className="mx-2" />
+            </a>
+            <a>
+              <PersonCircle size={20} color="white" className="ms-4 me-2" />
+            </a>
           </div>
         </Navbar.Collapse>
       </Container>
